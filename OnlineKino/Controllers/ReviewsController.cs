1931f216ea0497c788bac9fbbf5e14862ecdf296
Arthur Pak <sandtorus@gmail.com>
2025-04-08 03:56:38 +0500

    ***

--- OnlineKino/Controllers/ReviewsController.cs
+++ OnlineKino/Controllers/ReviewsController.cs
@@ -1,9 +1,11 @@
-﻿using Microsoft.AspNetCore.Http;
+﻿using ClosedXML.Excel;
+using Microsoft.AspNetCore.Http;
 using Microsoft.AspNetCore.Mvc;
 using Microsoft.EntityFrameworkCore;
 using OnlineKino.Context;
 using OnlineKino.Models;
 using OnlineKino.Services;
+using System.Data;
 
 namespace OnlineKino.Controllers
 {
@@ -90,7 +92,83 @@ namespace OnlineKino.Controllers
             return _context.Reviews.Any(e => e.id == id);
         }
 
+        [HttpGet("ExcelLastDay")]
+        public async Task<IActionResult> ExcelLastDay()
+        {
+            var reviews = await _context.Reviews
+                .FromSqlInterpolated($"EXEC pReviewsLast24")
+                .ToListAsync();
 
+            if (reviews == null || !reviews.Any())
+                return BadRequest();
 
+            using var workbook = new XLWorkbook();
+            var worksheet = workbook.Worksheets.Add("Data");
+            var dataTable = new DataTable();
+            var props = typeof(Reviews).GetProperties();
+
+            foreach (var prop in props)
+                dataTable.Columns.Add(prop.Name);
+
+            foreach (var item in reviews)
+            {
+                var row = dataTable.NewRow();
+                foreach (var prop in props)
+                {
+                    row[prop.Name] = prop.GetValue(item) ?? DBNull.Value;
+                }
+                dataTable.Rows.Add(row);
+            }
+
+            worksheet.Cell(1, 1).InsertTable(dataTable);
+            worksheet.Columns().AdjustToContents();
+
+            var stream = new MemoryStream();
+            workbook.SaveAs(stream);
+            stream.Position = 0;
+
+            return File(stream,
+                "application/vnd.openxmlformats-officedocument.spreadsheetml.sheet",
+                "Report.xlsx");
+        }
+        [HttpGet("ExcelLastMonth")]
+        public async Task<IActionResult> ExcelLastMonth()
+        {
+            var reviews = await _context.Reviews
+               .FromSqlInterpolated($"EXEC pReviewsLastMonth")
+               .ToListAsync();
+
+            if (reviews == null || !reviews.Any())
+                return BadRequest();
+
+            using var workbook = new XLWorkbook();
+            var worksheet = workbook.Worksheets.Add("Data");
+            var dataTable = new DataTable();
+            var props = typeof(Reviews).GetProperties();
+
+            foreach (var prop in props)
+                dataTable.Columns.Add(prop.Name);
+
+            foreach (var item in reviews)
+            {
+                var row = dataTable.NewRow();
+                foreach (var prop in props)
+                {
+                    row[prop.Name] = prop.GetValue(item) ?? DBNull.Value;
+                }
+                dataTable.Rows.Add(row);
+            }
+
+            worksheet.Cell(1, 1).InsertTable(dataTable);
+            worksheet.Columns().AdjustToContents();
+
+            var stream = new MemoryStream();
+            workbook.SaveAs(stream);
+            stream.Position = 0;
+
+            return File(stream,
+                "application/vnd.openxmlformats-officedocument.spreadsheetml.sheet",
+                "Report.xlsx");
+        }
     }
 }
--- OnlineKino/Services/ReviewService.cs
+++ OnlineKino/Services/ReviewService.cs
@@ -58,4 +58,5 @@ namespace OnlineKino.Services
             }
         }
     }
+
 }
